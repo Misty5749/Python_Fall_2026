4614d5540c2c59f1b9605a2906f5d839966db685
Austin <aminiur6327@students.mchenry.edu>
2026-09-01 13:53:01 -0500

    Refactor income handling to use net income and simplify expense calculations

--- Number_Formatting.py
+++ Number_Formatting.py
@@ -13,7 +13,7 @@ ASSIGNMENT REQUIREMENTS
 
 # user's monthly expenses
 name = input("Enter name: ")
-gross_income = float(input("Enter monthly gross income: "))
+income_net = float(input("Enter monthly net income: "))
 rent = float(input("Cost of rent monthly: "))
 utilities = float(input("Cost of utilities monthly: "))
 food = float(input("Cost of food monthly: "))
@@ -22,17 +22,8 @@ gas = float(input("Cost of gas monthly: "))
 
 
 # user's total income
-fed_tax = gross_income * 0.20
-net_income = gross_income - fed_tax
-income_rent = net_income - rent
-income_utilities = income_rent - utilities
-income_food = income_utilities - food
-income_hobbies = income_food - hobbies
-income_gas = income_hobbies - gas
+total_expenses = rent + utilities + food + hobbies + gas
 
 # table of expenses
-print(f"Gross Income: ${gross_income:,.2f}")
-print(f"Net Income: ${net_income:,.2f}")
-print(
-    f"Income after bills: ${income_rent:,.2f}\n{income_utilities:,.2f}\n{income_food:,.2f}\n{income_hobbies:,.2f}\n{income_gas:,.2f}"
-)
+print(f"Net Income: ${income_net:,.2f}")
+# print(f"Total Expenses"

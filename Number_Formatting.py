1da32db0dd16b3fb737e891d65b78506637fbb8b
Austin <aminiur6327@students.mchenry.edu>
2026-09-01 13:41:11 -0500

    Fix income calculations and update output formatting to display values in two decimal places

--- Number_Formatting.py
+++ Number_Formatting.py
@@ -24,8 +24,15 @@ gas = float(input("Cost of gas monthly: "))
 # user's total income
 fed_tax = gross_income * 0.20
 net_income = gross_income - fed_tax
+income_rent = net_income - rent
+income_utilities = income_rent - utilities
+income_food = income_utilities - food
+income_hobbies = income_food - hobbies
+income_gas = income_hobbies - gas
 
-#table of expenses
-print(f"Gross Income: ${gross_income,.2%}")
-print(f"Net Income: ${net_income,.2%}")
-print(f"Income after rent: ${net_income - rent,.2%}")
+# table of expenses
+print(f"Gross Income: ${gross_income:,.2f}")
+print(f"Net Income: ${net_income:,.2f}")
+print(
+    f"Income after bills: ${income_rent:,.2f}\n{income_utilities:,.2f}\n{income_food:,.2f}\n{income_hobbies:,.2f}\n{income_gas:,.2f}"
+)

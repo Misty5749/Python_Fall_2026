d7d06a4808d7d05c12235c439f96186dba9e75b9
Austin <aminiur6327@students.mchenry.edu>
2026-09-10 13:04:17 -0500

    Add initial implementation of loops.py with while and for loop tasks

---
+++ loops.py
@@ -0,0 +1,28 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Task 1: While Loop (The Nagging Kid)
+       - Repeats "Are we there yet?" until user types "yes".
+       - Uses a boolean variable to control the loop.
+[ ] 3. Task 2: For Loop (99 Bottles of Beer)
+       - Counts backwards from 99 to 1.
+       - Prints "[number] bottles of beer on the wall!"
+[ ] 4. Upload to GitHub and paste the link below.
+-----------------------------------------------------------------------
+"""
+
+# task 1 (Are we there yet)
+# creates a variable to keep track if the car is driving
+driving = True
+# user inputs if the car is still driving
+while driving:
+    answer = input("are we there yet (yes/no)").lower()
+    if answer == "yes":
+        driving = False
+
+# task 2 (99 bottles of beer)
+# counts down from 99 to 1 and sends one line for every time it goes down
+for count in range(99, 0, -1):
+    print(f"{count} bottles of beer on the wall. Take one and pass it around\n")
